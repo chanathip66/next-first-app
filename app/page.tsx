542ef7f162d68f3107c40a6271f0174db383542b
userrv6g8vin <userrvgvin@gmail.com>
2026-06-21 11:44:23 +0700

    add main page with owner info

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Page() {
+  return (
+    <div>
+      Hello....Main Page
+      <br />
+      ID: 6852D10099
+      <br />
+      Name: Chanintorn Chalermsuk
+    </div>
+  );
+}
